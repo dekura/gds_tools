e68e52b723fb9440b8cd22bc913ad81b9d85bf79
dekura <cgjhaha@qq.com>
2020-03-06 18:07:06 +0800

    feat: add scale gds

--- calibre_gds2oas/gds2gds_rule.py
+++ calibre_gds2oas/gds2gds_rule.py
@@ -1,12 +1,12 @@
 '''
 @Author: Guojin Chen
 @Date: 2019-11-13 02:53:10
-@LastEditTime: 2019-11-21 04:33:39
+@LastEditTime: 2020-03-02 17:31:28
 @Contact: cgjhaha@qq.com
-@Description: convert gds to oas
+@Description: convert gds to gds, why? using calibre to merge some areas.
 '''
 ### SRAF post processing rule
-def make_gds2oas_rule(gds_path, oas_path):
+def make_gds2gds_rule(gds_path, oas_path):
     drc_code = """
 LAYOUT PATH    '%s'
 """ % (gds_path)
@@ -14,7 +14,7 @@ LAYOUT PATH    '%s'
 LAYOUT PRIMARY '*'
 """
     drc_code += """
-DRC RESULTS DATABASE '%s' OASIS
+DRC RESULTS DATABASE '%s' GDSII
 """ % (oas_path)
     drc_code += """
 PRECISION 1000
@@ -22,16 +22,16 @@ LAYOUT SYSTEM GDSII
 DRC MAXIMUM RESULTS ALL
 DRC MAXIMUM VERTEX 4000
 
-LAYER MAP 0   datatype 0 1000 LAYER target     1000
-LAYER MAP 1   datatype 0 1001 LAYER lay_opc    1001
-LAYER MAP 4   datatype 0 1002 LAYER lay_sraf   1002
-LAYER MAP 50  datatype 0 1003 LAYER lay_gangt  1003
+LAYER MAP 2   datatype 0 1000 LAYER target     1000
+LAYER MAP 21   datatype 0 1001 LAYER lay_nosraf    1001
+LAYER MAP 22   datatype 0 1002 LAYER lay_bbox   1002
+// LAYER MAP 50  datatype 0 1003 LAYER lay_gangt  1003
 
 // #01: output results
-OUT_target     {COPY target     } DRC CHECK MAP OUT_target    0     0
-OUT_opc        {COPY lay_opc    } DRC CHECK MAP OUT_opc       1     0
-OUT_sraf       {COPY lay_sraf   } DRC CHECK MAP OUT_sraf      4     0
-OUT_gangt      {COPY lay_gangt  } DRC CHECK MAP OUT_gangt     50    0
+OUT_target     {COPY target     } DRC CHECK MAP OUT_target    2     0
+OUT_nosraf     {COPY lay_nosraf } DRC CHECK MAP OUT_nosraf    21    0
+OUT_bbox       {COPY lay_bbox   } DRC CHECK MAP OUT_bbox      22    0
+OUT_sraf       {not lay_nosraf lay_bbox  } DRC CHECK MAP OUT_sraf  23    0
 """
     return drc_code
 
---
+++ scale_gds/scale_gds.py
@@ -0,0 +1,118 @@
+#scale_gds.py by Haoyu
+###########################
+import gdspy
+import numpy as np
+import sys
+import os
+from tqdm import tqdm
+from progress.bar import Bar
+clipsize = 2048
+
+SCALE_LAYER = 40
+
+VIA_LAYER = 2
+NO_SRAF_LAYER = 21
+BBOX_LAYER = 22
+ALLOW_SRAF_LAYER = 23
+PRECISION = 1000
+
+def scale_rect_by_poly(poly):
+    xl = poly[0][0]  # 25
+    xr = poly[1][0]  # 75
+    yd = poly[0][1]  # 25
+    yu = poly[2][1]  # 75
+    point1 = np.array([xl-10/PRECISION, yd-10/PRECISION])
+    point2 = np.array([xr+10/PRECISION, yu+10/PRECISION])
+    return (point1, point2)
+
+def sraf_area_by_poly(poly):
+    xl = poly[0][0]  # 25
+    xr = poly[1][0]  # 75
+    yd = poly[0][1]  # 25
+    yu = poly[2][1]  # 75
+    point1 = np.array([xl-110/PRECISION, yd-110/PRECISION])
+    point2 = np.array([xr+110/PRECISION, yu+110/PRECISION])
+    return (point1, point2)
+
+def center_by_poly(poly):
+    xl = poly[0][0]  # 25
+    xr = poly[1][0]  # 75
+    yd = poly[0][1]  # 25
+    yu = poly[2][1]  # 75
+    x = (xl + xr)/2
+    y = (yd + yu)/2
+    center = np.array([x, y])
+    return center
+
+def scale_gds(Infolder, Infile, ImgOut):
+    GdsIn = os.path.join(Infolder, Infile)
+    gdsii   = gdspy.GdsLibrary()
+    lib_out = gdspy.GdsLibrary()
+
+    gdsii.read_gds(GdsIn,units='convert')
+    cell    = gdsii.top_level()[0]
+    cell_out = lib_out.new_cell('TOP')
+    bbox    = cell.get_bounding_box()
+    # bbox:  [[     0.      0.]
+    #        [148400. 146000.]]
+    # print('width: ', width)
+    # print('height: ', height)
+    bbox_out = gdspy.Rectangle(bbox[0],bbox[1], layer=BBOX_LAYER)
+    cell_out.add(bbox_out)
+    sellayer = [SCALE_LAYER] #Layer Number
+    dtype = 0  #Layout Data Type
+    token = 1
+    for i in tqdm(range(len(sellayer))):
+        try:
+            polyset = cell.get_polygons(by_spec=True)[(sellayer[i],dtype)]
+        except:
+            token=0
+            print("Layer not found, skipping...")
+            break
+        centers = np.zeros((len(polyset), 2))
+        for poly in range(0, len(polyset)):
+            # polyset[poly]
+            # [[121925.  56675.]
+            # [121975.  56675.]
+            # [121975.  56725.]
+            # [121925.  56725.]]
+            # print(polyset[poly][0])
+            rect_points = scale_rect_by_poly(polyset[poly])
+            sraf_points = sraf_area_by_poly(polyset[poly])
+            # print(rect_points[0])
+            rect = gdspy.Rectangle(rect_points[0], rect_points[1], layer=VIA_LAYER)
+            sraf_rect = gdspy.Rectangle(sraf_points[0], sraf_points[1], layer=NO_SRAF_LAYER)
+            center = center_by_poly(polyset[poly])
+            cell_out.add(rect)
+            cell_out.add(sraf_rect)
+            centers[poly] = center
+    if token == 1:
+        filename = Infile.replace('.gds', '_scaled.gds')
+        outpath  = os.path.join(ImgOut,filename)
+        lib_out.write_gds(outpath)
+        # txtname = Infile.replace('.gds', '_centers.txt')
+        # outtxtpath  = os.path.join(ImgOut,txtname)
+        # lib_out.write_gds(outpath)
+        # np.savetxt(outtxtpath, centers, fmt='%.5f', delimiter=' ')
+
+
+
+# Infolder = sys.argv[1]
+# Outfolder= sys.argv[2]
+dir = os.path.abspath(os.path.dirname(__file__))
+Infolder = os.path.join(dir,'input_gds')
+# Infolder = '/Users/dekura/Desktop/opc/design-april'
+Outfolder = os.path.join(dir,'output_gds')
+
+for dirname, dirnames, filenames in os.walk(Infolder):
+    bar=Bar("Scaling GDSII", max=len(filenames))
+    for f in range(0, len(filenames)):
+        scale_gds(Infolder, filenames[f], Outfolder)
+        # try:
+        #     scale_gds(Infolder, filenames[f], Outfolder)
+        # except:
+        #     bar.next()
+        #     continue
+        bar.next()
+bar.finish()
+print('all rect scaled')
