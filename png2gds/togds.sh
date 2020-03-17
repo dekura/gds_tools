2041342c88ef94800ff1899dd95dc1f2b65659be
dekura <cgjhaha@qq.com>
2020-03-17 14:27:34 +0800

    💪 WIP:

--- png2gds/togds.sh
+++ png2gds/togds.sh
@@ -1,7 +1,7 @@
 ### 
 # @Author: Guojin Chen
  # @Date: 2019-11-19 12:51:07
- # @LastEditTime: 2020-03-17 14:09:54
+ # @LastEditTime: 2020-03-17 14:27:18
  # @Contact: cgjhaha@qq.com
  # @Description:
  ###
@@ -19,13 +19,13 @@ python=/home/glchen/miniconda3/envs/py3/bin/python
 # python=/usr/local/miniconda3/envs/pytorch/bin/python
 $python paired2gds_nobbox.py \
 --name layouts05frac48via12 \
---in_folder  \
---out_folder /Users/dekura/Downloads/testnewpng2gdsout/ \
+--in_folder /home/glchen/datasets/dmo_results_splited \
+--out_folder /home/glchen/datasets/dmo_results2gds \
 --img_size 2048 \
 --threshold 0.6 \
 --split_id $sid \
---fake_B_postname _mbsraf_mb_mb_lccout.oas_fake_B.png \
---real_A_postname _mbsraf_mb_mb_lccout.oas.png
+--fake_B_postname _mbsraf.gds_lccout_CALI_fake_B.png \
+--real_A_postname _mbsraf.gds_lccout_CALI.png
 
 # $python paired2gds_nobbox.py \
 # --name via04dmo1024 \
