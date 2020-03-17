bd75fe3e774fe139ffd60493b8205a0cb9c48e44
dekura <cgjhaha@qq.com>
2020-03-17 14:20:15 +0800

    💪 WIP:

--- png2gds/split_results.sh
+++ png2gds/split_results.sh
@@ -1,7 +1,7 @@
 ### 
 # @Author: Guojin Chen
  # @Date: 2019-11-19 20:45:42
- # @LastEditTime: 2020-03-17 14:11:03
+ # @LastEditTime: 2020-03-17 14:20:06
  # @Contact: cgjhaha@qq.com
  # @Description:
  ###
@@ -12,7 +12,7 @@ $python split_results.py \
 --in_real_folder /home/glchen/datasets/dmo_results/layouts05frac48via12_e100_dr2mg_1024_1024/testbg \
 --out_folder /home/glchen/datasets/dmo_results_splited \
 --split_num 8 \
---real_A_postname mbsraf.gds_lccout_CALI.png
+--real_A_postname _mbsraf.gds_lccout_CALI.png
 
 
 
