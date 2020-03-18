94e48e742617f9de3c774179556a1f3ef33e02d0
dekura <cgjhaha@qq.com>
2020-03-18 10:32:49 +0800

    💪 WIP:

--- png2gds/split_results.sh
+++ png2gds/split_results.sh
@@ -1,15 +1,15 @@
-### 
+###
 # @Author: Guojin Chen
  # @Date: 2019-11-19 20:45:42
- # @LastEditTime: 2020-03-17 14:20:06
+ # @LastEditTime: 2020-03-18 10:32:37
  # @Contact: cgjhaha@qq.com
  # @Description:
  ###
 python=/home/glchen/miniconda3/envs/py3/bin/python
 $python split_results.py \
---name layouts05frac48via12 \
---in_folder /home/glchen/datasets/dmo_results/layouts05frac48via12_e100_dr2mg_1024_1024/test_latest/images \
---in_real_folder /home/glchen/datasets/dmo_results/layouts05frac48via12_e100_dr2mg_1024_1024/testbg \
+--name ovia1_e70_dr2mg \
+--in_folder /home/glchen/datasets/dmo_results/ovia1_e70_dr2mg/test_latest/images \
+--in_real_folder /home/glchen/datasets/dmo_results/ovia1_e70_dr2mg/testbg \
 --out_folder /home/glchen/datasets/dmo_results_splited \
 --split_num 8 \
 --real_A_postname _mbsraf.gds_lccout_CALI.png
